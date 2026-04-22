db36bf6b146a6e16baf3bf5ac2fe169bb87f38a2
Mandana Dashti <dmandana59@gmail.com>
2026-04-22 20:42:32 +0330

    feat: create JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,15 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from '../users/users.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [
+    TypeOrmModule.forFeature([Users]),
+    JwtModule.register({
+      secret: 'super-secret-key',
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
