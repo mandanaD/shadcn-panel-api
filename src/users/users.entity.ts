faee3c51e0b024e8c67f08dff26aa3a024220de3
Mandana Dashti <dmandana59@gmail.com>
2026-04-22 19:59:45 +0330

    feat: create users entity

---
+++ src/users/users.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  first_name: string;
+  @Column()
+  last_name: string;
+  @Column({ unique: true })
+  email: string;
+  @Column()
+  password: string;
+  @Column({ nullable: true })
+  bio: string;
+  @Column({ default: false })
+  is_admin: boolean;
+  @CreateDateColumn()
+  created_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
+}
