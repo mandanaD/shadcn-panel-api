ba1ee50b1144e4b318f0dfd30e3d3c489d442987
Mandana Dashti <dmandana59@gmail.com>
2026-04-22 19:55:50 +0330

    feat: create signup and login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dtos/login.dto';
+import { SignupDto } from './dtos/signup.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('signup')
+  signUp(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto);
+  }
+
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Post('refresh')
+  refresh() {
+    return 'refresh';
+  }
+
+  @Post('logout')
+  logout() {
+    return 'logout';
+  }
+}
