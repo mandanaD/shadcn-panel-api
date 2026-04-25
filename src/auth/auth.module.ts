7722b8028648f3da98cde87ac2dc9b5a90d283e9
Mandana Dashti <dmandana59@gmail.com>
2026-04-25 22:32:55 +0330

    feat: add RefreshToken entity

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,15 +4,17 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from '../users/users.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { RefreshToken } from './refresh-token.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Users]),
+    TypeOrmModule.forFeature([Users, RefreshToken]),
     JwtModule.register({
       secret: 'super-secret-key',
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
