49ad8bf43a1ed570ab58977c5a54cf9bbfcbc0f6
Mandana Dashti <dmandana59@gmail.com>
2026-04-25 17:50:21 +0330

    feat: add profile get route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { Users } from './users.entity';
+import { JwtGuard } from '../auth/jwt.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @UseGuards(JwtGuard)
+  @Get('profile')
+  getProfile(@CurrentUser() user: Partial<Users>) {
+    return this.usersService.getProfile(user.id || '');
+  }
 }
