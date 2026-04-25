c130045812a0ed664bf3402f5e329408ff9dcf9b
Mandana Dashti <dmandana59@gmail.com>
2026-04-25 17:48:08 +0330

    feat: create JwtStrategy tto check HeaderAsBearerToken and validate token

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { payloadType } from './auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'super-secret-key',
+    });
+  }
+  // this return as req.user so we can access it with @Req
+  validate(payload: payloadType) {
+    return {
+      id: payload.sub,
+      email: payload.email,
+      is_admin: payload.is_admin,
+    };
+  }
+}
