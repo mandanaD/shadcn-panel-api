e91f0b568f4611557c175059364972696cf6dc45
Mandana Dashti <dmandana59@gmail.com>
2026-04-25 22:34:49 +0330

    feat: add swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,24 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  // swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Shadcn-Panel-Api')
+    .setDescription('API documentation for Shadcn-Panel-Api project')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  //
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
