162cf41e2fc022ce6479713dd91aec3adec2666c
Mandana Dashti <dmandana59@gmail.com>
2026-04-25 22:35:03 +0330

    feat: add swagger property

--- src/auth/dtos/signup.dto.ts
+++ src/auth/dtos/signup.dto.ts
@@ -1,12 +1,17 @@
 import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SignupDto {
   @IsString()
+  @ApiProperty()
   first_name: string;
   @IsString()
+  @ApiProperty()
   last_name: string;
   @IsEmail()
+  @ApiProperty()
   email: string;
   @IsString()
+  @ApiProperty()
   password: string;
 }
