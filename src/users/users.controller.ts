30724c1625e574b06a16807c617c6bdb2c7ed765
Mandana Dashti <dmandana59@gmail.com>
2026-04-26 15:27:47 +0330

    feat: add users list and user crud route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,63 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { Users } from './users.entity';
 import { JwtGuard } from '../auth/jwt.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { GetUsersQueryDto } from './dtos/get-users-query.dto';
+import { UpdateProfileDto } from './dtos/update-profile.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
+@UseGuards(JwtGuard)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @UseGuards(JwtGuard)
+  @Get('')
+  @ApiBearerAuth('access-token')
+  getUsers(@Query() query: GetUsersQueryDto) {
+    return this.usersService.getUsers(query);
+  }
+
+  @Get(':id')
+  @ApiBearerAuth('access-token')
+  getUser(@Param('id') id: string) {
+    return this.usersService.getUser(id);
+  }
+
+  @Patch(':id')
+  @ApiBearerAuth('access-token')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.updateUser(id, body);
+  }
+
+  @Delete(':id')
+  @ApiBearerAuth('access-token')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.deleteUser(id);
+  }
+
   @Get('profile')
+  @ApiBearerAuth('access-token')
   getProfile(@CurrentUser() user: Partial<Users>) {
     return this.usersService.getProfile(user.id || '');
+  }
+
+  @Patch('profile')
+  @ApiBearerAuth('access-token')
+  updateProfile(
+    @Body() body: UpdateProfileDto,
+    @CurrentUser() user: Partial<Users>,
+  ) {
+    return this.usersService.updateProfile(user.id || '', body);
   }
 }
