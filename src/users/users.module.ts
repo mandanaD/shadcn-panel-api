6d0757f7fa9645ce735090e22095c7d655019299
Mandana Dashti <dmandana59@gmail.com>
2026-04-26 15:28:23 +0330

    feat: add Users entity

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { JwtStrategy } from '../auth/jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Users])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, JwtStrategy],
 })
 export class UsersModule {}
