5c0155b864a3c6421f12e289d6e8ff746ab490fe
Mandana Dashti <dmandana59@gmail.com>
2026-04-29 14:07:30 +0330

    feat: add ApiProperty decorator for swagger

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,20 +1,29 @@
 import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class User {
   @Expose()
+  @ApiProperty()
   id: string;
   @Expose()
+  @ApiProperty()
   first_name: string;
   @Expose()
+  @ApiProperty()
   last_name: string;
   @Expose()
+  @ApiProperty()
   email: string;
   @Expose()
+  @ApiProperty()
   bio: string;
   @Expose()
+  @ApiProperty()
   is_admin: boolean;
   @Expose()
+  @ApiProperty()
   created_at: Date;
   @Expose()
+  @ApiProperty()
   updated_at: Date;
 }
