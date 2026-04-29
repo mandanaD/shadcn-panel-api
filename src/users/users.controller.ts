f9d97be31ab10755f09887289c94fd0f682071dd
Mandana Dashti <dmandana59@gmail.com>
2026-04-29 14:07:44 +0330

    feat: add ApiResponse

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -18,6 +18,7 @@ import { UpdateProfileDto } from './dtos/update-profile.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { User } from './dtos/user.dto';
+import { ApiResponse } from '../decorator/api-response.decorator';
 
 @Controller('users')
 @UseGuards(JwtGuard)
@@ -27,36 +28,42 @@ export class UsersController {
 
   @Get('')
   @ApiBearerAuth('access-token')
+  @ApiResponse({ dto: User, isArray: true })
   getUsers(@Query() query: GetUsersQueryDto) {
     return this.usersService.getUsers(query);
   }
 
   @Get(':id')
   @ApiBearerAuth('access-token')
+  @ApiResponse({ dto: User })
   getUser(@Param('id') id: string) {
     return this.usersService.getUser(id);
   }
 
   @Patch(':id')
   @ApiBearerAuth('access-token')
+  @ApiResponse({ dto: User })
   updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.usersService.updateUser(id, body);
   }
 
   @Delete(':id')
   @ApiBearerAuth('access-token')
+  @ApiResponse({ dto: User })
   deleteUser(@Param('id') id: string) {
     return this.usersService.deleteUser(id);
   }
 
   @Get('profile')
   @ApiBearerAuth('access-token')
+  @ApiResponse({ dto: User })
   getProfile(@CurrentUser() user: Partial<Users>) {
     return this.usersService.getProfile(user.id || '');
   }
 
   @Patch('profile')
   @ApiBearerAuth('access-token')
+  @ApiResponse({ dto: User })
   updateProfile(
     @Body() body: UpdateProfileDto,
     @CurrentUser() user: Partial<Users>,
