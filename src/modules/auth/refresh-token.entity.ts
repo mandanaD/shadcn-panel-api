1e33f63609e5009b3d84cb7273ae22ba35e73d2b
Mandana Dashti <dmandana59@gmail.com>
2026-05-03 13:57:31 +0330

    feat: add BaseEntity

--- src/modules/auth/refresh-token.entity.ts
+++ src/modules/auth/refresh-token.entity.ts
@@ -1,32 +1,14 @@
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
-import { Users } from '../users/users.entity';
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from '../../entity/base.entity';
 
 @Entity('refresh_tokens')
-export class RefreshToken {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
+export class RefreshToken extends BaseEntity {
   @Column()
   token: string;
 
   @Column({ type: 'timestamp' })
   expires_at: Date;
 
-  @CreateDateColumn()
-  created_at: Date;
-
   @Column({ default: false })
   revoked: boolean;
-
-  @ManyToOne(() => Users, (users) => users.refreshTokens, {
-    onDelete: 'CASCADE',
-    // if user deleted then delete this token as well
-  })
-  user: Users;
 }
