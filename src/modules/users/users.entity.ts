4774fdef0b064c482d910018ccdbcf832728f58b
Mandana Dashti <dmandana59@gmail.com>
2026-05-03 13:57:02 +0330

    feat: add Ticket relation

--- src/modules/users/users.entity.ts
+++ src/modules/users/users.entity.ts
@@ -2,32 +2,57 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { RefreshToken } from '../auth/refresh-token.entity';
+import { Ticket } from '../ticket/entity/ticket.entity';
 
 @Entity('users')
 export class Users {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
   @Column()
   first_name: string;
+
   @Column()
   last_name: string;
+
   @Column({ unique: true })
   email: string;
+
   @Column()
   password: string;
+
   @Column({ nullable: true })
   bio: string;
+
   @Column({ default: false })
   is_admin: boolean;
+
   @CreateDateColumn()
   created_at: Date;
+
   @UpdateDateColumn()
   updated_at: Date;
-  @OneToMany(() => RefreshToken, (token) => token.user)
+
+  @ManyToOne(() => Users, { onDelete: 'SET NULL', nullable: true })
+  @JoinColumn({ name: 'created_by' })
+  @Index()
+  created_by: Users;
+
+  @ManyToOne(() => Users, { onDelete: 'SET NULL', nullable: true })
+  @JoinColumn({ name: 'updated_by' })
+  updated_by: Users;
+
+  @OneToMany(() => RefreshToken, (token) => token.created_by)
   refreshTokens: RefreshToken[];
+
+  @OneToMany(() => Ticket, (ticket) => ticket.owner)
+  tickets: Ticket[];
 }
