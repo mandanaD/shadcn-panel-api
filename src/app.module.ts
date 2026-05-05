7dc610ab009c36c55868cbc073ea3b9f7bae3f17
Mandana Dashti <dmandana59@gmail.com>
2026-05-05 21:08:08 +0330

    refactor: add jwtGuard and AuditInterceptor(saving userId in AsyncLocalStorage context) globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,23 @@ import { UsersModule } from './modules/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './modules/users/users.entity';
 import { TicketModule } from './modules/ticket/ticket.module';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { JwtGuard } from './modules/auth/jwt/jwt.guard';
+import { AuditSubscriber } from './audit/audit.user-subscriber';
+import { AuditInterceptor } from './audit/audit.interceptor';
 
 @Module({
+  providers: [
+    AuditSubscriber,
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AuditInterceptor,
+    },
+  ],
   imports: [
     AuthModule,
     UsersModule,
