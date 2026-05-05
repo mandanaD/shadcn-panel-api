ab3eaa852cb5ce7f43b1889ccac888a25443d5b9
Mandana Dashti <dmandana59@gmail.com>
2026-05-05 21:18:37 +0330

    feat: import UsersModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,9 +6,11 @@ import { Users } from '../users/users.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt/jwt.strategy';
 import { RefreshToken } from './refresh-token.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     TypeOrmModule.forFeature([Users, RefreshToken]),
     JwtModule.register({
       secret: 'super-secret-key',
