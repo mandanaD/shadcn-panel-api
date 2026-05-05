ef793e89d517a10057a4c803ff829a3d3a5cf654
Mandana Dashti <dmandana59@gmail.com>
2026-05-05 21:18:26 +0330

    feat: add public decorator

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,39 +1,39 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/login.dto';
 import { SignupDto } from './dtos/signup.dto';
 import { RefreshDto } from './dtos/refresh.dto';
-import { JwtGuard } from './jwt/jwt.guard';
 import { AuthResponseDto, TokenPairDto } from './dtos/auth-response.dto';
 import {
   ApiCreatedResponse,
   ApiNoContentResponse,
   ApiOkResponse,
 } from '@nestjs/swagger';
+import { Public } from '../../decorator/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
+  @Public()
   @ApiCreatedResponse({ type: AuthResponseDto })
   signUp(@Body() signupDto: SignupDto) {
     return this.authService.signup(signupDto);
   }
 
   @Post('login')
+  @Public()
   @ApiOkResponse({ type: AuthResponseDto })
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
-  @UseGuards(JwtGuard)
   @Post('refresh')
   @ApiOkResponse({ type: TokenPairDto })
   refresh(@Body() refresh: RefreshDto) {
     return this.authService.refresh(refresh.refresh_token);
   }
 
-  @UseGuards(JwtGuard)
   @Post('logout')
   @ApiNoContentResponse()
   logout(@Body() refresh: RefreshDto) {
