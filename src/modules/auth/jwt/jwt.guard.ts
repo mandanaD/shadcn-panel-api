d3fd0e10ee94273962e099555aab2e3bdc4b7dbe
Mandana Dashti <dmandana59@gmail.com>
2026-05-05 21:12:03 +0330

    feat: check if route have @Public then dont validate

--- src/modules/auth/jwt/jwt.guard.ts
+++ src/modules/auth/jwt/jwt.guard.ts
@@ -1,5 +1,26 @@
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { IS_PUBLIC_KEY } from '../../../decorator/public.decorator';
 
 // reject invalid or expired tokens
 // will look at the Authorization: Bearer <token> header
-export class JwtGuard extends AuthGuard('jwt') {}
+@Injectable()
+export class JwtGuard extends AuthGuard('jwt') {
+  // reflector allow use to access the metadata pass by custom decorator to route or class
+  constructor(private reflector: Reflector) {
+    super();
+  }
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+}
