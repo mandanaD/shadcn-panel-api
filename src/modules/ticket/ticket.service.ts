7a8df91ce92ec569b03e876735bfa3c3a2e160ef
Mandana Dashti <dmandana59@gmail.com>
2026-05-07 17:07:34 +0330

    feat: create TicketService

--- src/modules/ticket/ticket.service.ts
+++ src/modules/ticket/ticket.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Ticket } from './entity/ticket.entity';
+import { Repository } from 'typeorm';
+import { Message } from './entity/message.entity';
+import { CreateTicketDto } from './dtos/create-ticket.dto';
+import { Users } from '../users/users.entity';
+import { UpdateTicketDto } from './dtos/update-ticket.dto';
 
 @Injectable()
-export class TicketService {}
+export class TicketService {
+  constructor(
+    @InjectRepository(Ticket) private ticketRepo: Repository<Ticket>,
+    @InjectRepository(Message) private messageRepo: Repository<Message>,
+  ) {}
+  async getTicket(id: string) {
+    const ticket = await this.ticketRepo.findOne({
+      where: { id },
+      relations: ['messages', 'owner'],
+    });
+    if (ticket) {
+      return ticket;
+    }
+    throw new NotFoundException('Ticket not found');
+  }
+  async createTicket(body: CreateTicketDto, user: Partial<Users>) {
+    const ticketBody = this.ticketRepo.create({
+      ...body,
+      owner: { id: body.owner || user.id } as Users,
+    });
+    const ticket = await this.ticketRepo.save(ticketBody);
+
+    const messageBody = this.messageRepo.create({
+      body: body.message,
+      ticket: ticket,
+    });
+    await this.messageRepo.save(messageBody);
+
+    return this.getTicket(ticket.id);
+  }
+
+  async updateTicket(id: string, body: UpdateTicketDto) {
+    const ticket = await this.ticketRepo.findOne({
+      where: { id },
+    });
+    if (!ticket) {
+      throw new NotFoundException(`Ticket not found`);
+    } else {
+      const ticketBody = Object.assign(ticket, body);
+      return await this.ticketRepo.save(ticketBody);
+    }
+  }
+
+  getTickets() {
+    return this.ticketRepo.find();
+  }
+
+  async getTicketMessages(id: string) {
+    const ticket = await this.getTicket(id);
+    if (ticket) {
+      return this.messageRepo.find({
+        where: { ticket_id: ticket.id },
+      });
+    }
+  }
+}
