2b31a27373d631a53352c353b7daff35291c9718
Mandana Dashti <dmandana59@gmail.com>
2026-05-07 17:07:56 +0330

    feat: add entities

--- src/modules/ticket/ticket.module.ts
+++ src/modules/ticket/ticket.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TicketService } from './ticket.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ticket } from './entity/ticket.entity';
+import { Message } from './entity/message.entity';
+import { TicketController } from './ticket.controller';
+import { JwtStrategy } from '../auth/jwt/jwt.strategy';
 
 @Module({
-  providers: [TicketService]
+  imports: [TypeOrmModule.forFeature([Ticket, Message])],
+  controllers: [TicketController],
+  providers: [TicketService, JwtStrategy],
 })
 export class TicketModule {}
