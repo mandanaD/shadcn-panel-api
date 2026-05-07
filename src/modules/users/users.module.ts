7fc9c649f310598c113263d84dd511c99c0d494b
Mandana Dashti <dmandana59@gmail.com>
2026-05-07 16:21:37 +0330

    feat: import AddressModule

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,9 +4,11 @@ import { UsersService } from './users.service';
 import { JwtStrategy } from '../auth/jwt/jwt.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './users.entity';
+import { AddressModule } from '../address/address.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [TypeOrmModule.forFeature([Users]), AddressModule],
+  exports: [UsersService],
   controllers: [UsersController],
   providers: [UsersService, JwtStrategy],
 })
