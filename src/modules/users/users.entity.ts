ec6623fd2d4afaefa8d6db0e32e23fa96c7efae9
Mandana Dashti <dmandana59@gmail.com>
2026-05-07 16:22:15 +0330

    feat: add created_by and _created_by

--- src/modules/users/users.entity.ts
+++ src/modules/users/users.entity.ts
@@ -7,10 +7,13 @@ import {
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
+  RelationId,
   UpdateDateColumn,
 } from 'typeorm';
 import { RefreshToken } from '../auth/refresh-token.entity';
 import { Ticket } from '../ticket/entity/ticket.entity';
+import { State } from '../address/entity/state.entity';
+import { City } from '../address/entity/city.entity';
 
 @Entity('users')
 export class Users {
@@ -35,6 +38,24 @@ export class Users {
   @Column({ default: false })
   is_admin: boolean;
 
+  @ManyToOne(() => State, {
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn({ name: 'state_id' })
+  state?: State | null;
+
+  @RelationId((s: Users) => s.state)
+  state_id: string;
+
+  @ManyToOne(() => City, {
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn({ name: 'city_id' })
+  city?: City | null;
+
+  @RelationId((s: Users) => s.city)
+  city_id: string;
+
   @CreateDateColumn()
   created_at: Date;
 
@@ -44,13 +65,19 @@ export class Users {
   @ManyToOne(() => Users, { onDelete: 'SET NULL', nullable: true })
   @JoinColumn({ name: 'created_by' })
   @Index()
-  created_by: Users;
+  _created_by: Users;
+
+  @RelationId((entity: Users) => entity._created_by)
+  created_by: string;
 
   @ManyToOne(() => Users, { onDelete: 'SET NULL', nullable: true })
   @JoinColumn({ name: 'updated_by' })
-  updated_by: Users;
+  _updated_by: Users;
 
-  @OneToMany(() => RefreshToken, (token) => token.created_by)
+  @RelationId((entity: Users) => entity._updated_by)
+  updated_by: string;
+
+  @OneToMany(() => RefreshToken, (token) => token._created_by)
   refreshTokens: RefreshToken[];
 
   @OneToMany(() => Ticket, (ticket) => ticket.owner)
