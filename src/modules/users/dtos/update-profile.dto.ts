ea4712d0aa0318c4d5bc2e0bd426da498121d4d6
Mandana Dashti <dmandana59@gmail.com>
2026-05-07 17:07:04 +0330

    feat: add state_id & city_id

--- src/modules/users/dtos/update-profile.dto.ts
+++ src/modules/users/dtos/update-profile.dto.ts
@@ -1,14 +1,29 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString, IsUUID } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateProfileDto {
   @IsString()
+  @IsOptional()
   @ApiProperty()
-  first_name: string;
+  first_name?: string;
+
   @IsString()
+  @IsOptional()
   @ApiProperty()
-  last_name: string;
+  last_name?: string;
+
   @IsString()
+  @IsOptional()
   @ApiProperty()
-  bio: string;
+  bio?: string;
+
+  @IsUUID()
+  @IsOptional()
+  @ApiProperty()
+  state_id?: string;
+
+  @IsUUID()
+  @IsOptional()
+  @ApiProperty()
+  city_id?: string;
 }
