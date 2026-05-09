19fc2cebacd679438fac62da090a3b7500bb5df1
Mandana Dashti <dmandana59@gmail.com>
2026-05-09 21:06:32 +0330

    refactor: add cities & states

--- src/modules/address/address.controller.ts
+++ src/modules/address/address.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { AddressService } from './address.service';
 import { Public } from '../../decorator/public.decorator';
+import { CpStateDto } from './dtos/cp-state.dto';
+import { CPCityDto } from './dtos/cp-city.dto';
 
 @Controller('address')
 export class AddressController {
@@ -12,9 +22,57 @@ export class AddressController {
     return this.addressService.getStates();
   }
 
+  @Post('states')
+  @Public()
+  createState(@Body() body: CpStateDto) {
+    return this.addressService.createState(body);
+  }
+
+  @Get('states/:id')
+  @Public()
+  getState(@Param('id') id: string) {
+    return this.addressService.getState(id);
+  }
+
+  @Patch('states/:id')
+  @Public()
+  editState(@Param('id') id: string, @Body() state: CpStateDto) {
+    return this.addressService.editState(id, state);
+  }
+
+  @Delete('states/:id')
+  @Public()
+  deleteState(@Param('id') id: string) {
+    return this.addressService.deleteState(id);
+  }
+
   @Get('cities')
   @Public()
   getCities() {
     return this.addressService.getCities();
+  }
+
+  @Post('cities')
+  @Public()
+  createCity(@Body() body: CPCityDto) {
+    return this.addressService.createCity(body);
+  }
+
+  @Get('cities/:id')
+  @Public()
+  getCity(@Param('id') id: string) {
+    return this.addressService.getCity(id);
+  }
+
+  @Patch('cities/:id')
+  @Public()
+  editCity(@Param('id') id: string, @Body() state: CPCityDto) {
+    return this.addressService.editCity(id, state);
+  }
+
+  @Delete('cities/:id')
+  @Public()
+  deleteCity(@Param('id') id: string) {
+    return this.addressService.deleteCity(id);
   }
 }
