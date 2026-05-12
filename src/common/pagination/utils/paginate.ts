a5e2a627b755964c44c668a6e1faaa63f2554b00
Mandana Dashti <dmandana59@gmail.com>
2026-05-13 01:24:08 +0330

    feat: create Paginate

---
+++ src/common/pagination/utils/paginate.ts
@@ -0,0 +1,29 @@
+import { ObjectLiteral, SelectQueryBuilder } from 'typeorm';
+import type {
+  PaginationInterface,
+  PaginationOptions,
+} from '../type/pagination.interface';
+
+export const Paginate = async <T extends ObjectLiteral>(
+  queryBuilder: SelectQueryBuilder<T>,
+  options: PaginationOptions,
+): Promise<PaginationInterface<T>> => {
+  const limit = options.limit || 10;
+  const page = options.page || 1;
+
+  const skip = (page - 1) * limit;
+
+  const [data, total] = await queryBuilder
+    .skip(skip)
+    .take(limit)
+    .getManyAndCount();
+
+  return {
+    items: data,
+    current_page: page,
+    current_page_items_count: data?.length,
+    items_per_page: limit,
+    pages_count: Math.ceil(total / limit),
+    total_items: total,
+  };
+};
