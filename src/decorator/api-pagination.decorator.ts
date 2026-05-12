aa0a0d392db5bbe381aba97c7b49a8d7db3d938b
Mandana Dashti <dmandana59@gmail.com>
2026-05-13 01:24:20 +0330

    feat: create ApiPagination

---
+++ src/decorator/api-pagination.decorator.ts
@@ -0,0 +1,28 @@
+import { applyDecorators, Type } from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+
+import { PaginationResponseDto } from '../common/pagination/dto/pagination-response.dto';
+
+export function ApiPagination<T extends Type<any>>(model: T) {
+  return applyDecorators(
+    ApiExtraModels(PaginationResponseDto, model),
+
+    ApiOkResponse({
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(PaginationResponseDto) },
+          {
+            properties: {
+              items: {
+                type: 'array',
+                items: {
+                  $ref: getSchemaPath(model),
+                },
+              },
+            },
+          },
+        ],
+      },
+    }),
+  );
+}
