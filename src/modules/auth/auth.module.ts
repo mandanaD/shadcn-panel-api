a0d3358ed944ee6cc61b6bb3203bddf553bd00ca
Mandana Dashti <dmandana59@gmail.com>
2026-05-17 15:10:34 +0330

    feat: add env

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -7,13 +7,18 @@ import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt/jwt.strategy';
 import { RefreshToken } from './refresh-token.entity';
 import { UsersModule } from '../users/users.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     TypeOrmModule.forFeature([Users, RefreshToken]),
-    JwtModule.register({
-      secret: 'super-secret-key',
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
     }),
   ],
   controllers: [AuthController],
